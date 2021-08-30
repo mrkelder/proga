21ea2c7043c864117381ced8cfc49b457308ac53
mrkelder <vageev06@gmail.com>
2021-08-30 16:35:26 +0300

    Prerequisites

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      red: colors.rose,
+      yellow: colors.amber,
+      primary: "#7c3aed",
+      secondary: "#5b21b6",
+      success: "#10b981",
+      error: "#f44336"
+    }
+  }
+}
