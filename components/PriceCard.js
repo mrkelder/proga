f3dfc34d9b5ebf5ba26036bbec22536eaa9b77aa
mrkelder <vageev06@gmail.com>
2021-09-13 17:46:50 +0300

    Made PriceCard's svg icon flexible

--- components/PriceCard.js
+++ components/PriceCard.js
@@ -31,12 +31,11 @@ function Card({ price, title, text, pluses, fS }) {
                 >
                   <div className="flex-shrink-0">
                     <svg
-                      className="h-6 w-6 mr-2"
+                      className="h-6 w-6 mr-2 text-success"
                       xmlns="http://www.w3.org/2000/svg"
                       width="6"
                       height="6"
-                      stroke="currentColor"
-                      fill="#10b981"
+                      fill="currentColor"
                       viewBox="0 0 1792 1792"
                     >
                       <path d="M1412 734q0-28-18-46l-91-90q-19-19-45-19t-45 19l-408 407-226-226q-19-19-45-19t-45 19l-91 90q-18 18-18 46 0 27 18 45l362 362q19 19 45 19 27 0 46-19l543-543q18-18 18-45zm252 162q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"></path>
@@ -75,7 +74,7 @@ Card.defaultProps = {
   title: "Сайт",
   text: "Start selling online for free with all the features you need to launch your local delivery and pick-up service, nothing more. We don't charge commission or monthly fees, keep all your margin.",
   pluses: new Array(5).fill("All illimited components"),
-  fS: "48px",
+  fS: "48px"
 };
 
 export default Card;
