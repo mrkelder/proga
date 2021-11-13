939f4ade736ff5517254df88ace91dc527bac271
mrkelder <vageev06@gmail.com>
2021-11-13 14:06:51 +0300

    Loading optimization

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
+import dynamic from "next/dynamic";
 import Header from "components/Header";
 import AboutUs from "components/AboutUs";
-import WhyUs from "components/WhyUs";
-import Statistics from "components/Statistics";
-import Prices from "components/Prices";
-import Footer from "components/Footer";
-import Dialog from "components/Dialog";
 import { useState } from "react";
+
+const WhyUs = dynamic(() => import("components/WhyUs"));
+const Statistics = dynamic(() => import("components/Statistics"));
+const Prices = dynamic(() => import("components/Prices"));
+const Footer = dynamic(() => import("components/Footer"));
+const Dialog = dynamic(() => import("components/Dialog"));
 
 export default function Home() {
   const [isVisible, setVisibility] = useState(false);
@@ -15,14 +17,14 @@ export default function Home() {
       <Head>
         <title>Proga</title>
       </Head>
-
-      <Dialog {...{ isVisible, setVisibility }} />
-
-      <Header {...{ setVisibility }} />
-      <AboutUs />
-      <WhyUs />
-      <Statistics />
-      <Prices />
+      <main>
+        <Dialog {...{ isVisible, setVisibility }} />
+        <Header {...{ setVisibility }} />
+        <AboutUs />
+        <WhyUs />
+        <Statistics />
+        <Prices />
+      </main>
       <Footer />
     </>
   );
