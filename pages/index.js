0a851a54342215458f6a2b258d70bf77f494704e
mrkelder <vageev06@gmail.com>
2021-11-15 11:04:10 +0300

    Added necessary meta tags

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,33 @@ const Dialog = dynamic(() => import("components/Dialog"));
 
 export default function Home() {
   const [isVisible, setVisibility] = useState(false);
+  const metaTitle = "Proga - разработка современных веб-приложений.";
+  const metaDescription =
+    "Proga - разработка современных веб-приложений, способных повысить продажи, увеличить аудиторию или улучшить производительность имеющегося продукта.";
+  const metaLogo = "/img/logo.jpg";
   return (
     <>
+      {/* TODO: metatags, semmantics (check headers and check sections), image sizing optimization */}
       <Head>
         <title>Proga</title>
+        <meta name="description" content={metaDescription} />
+        <meta
+          name="keywords"
+          content="Создание сайтов, разработка сайтов, создание интернет-магазин, создать интернет-магазин"
+        />
+        <meta name="author" content="Proga" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Social media meta */}
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={metaLogo} />
+        <meta property="og:url" content="/" />
+        <meta name="twitter:card" content={metaLogo} />
+
+        <meta property="og:description" content={metaDescription} />
+        <meta property="og:site_name" content={metaTitle} />
+        <meta name="twitter:image:alt" content={metaTitle} />
       </Head>
       <Dialog {...{ isVisible, setVisibility }} />
       <Header {...{ setVisibility }} />
